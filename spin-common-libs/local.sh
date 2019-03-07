0bd843aec7abadd6db1baac4f4c8a29316a9328f
Mustafa Morca <psychoplasma@gmail.com>
2019-03-07 17:53:36 +0900

    Fix local deployment errors

--- spin-common-libs/local.sh
+++ spin-common-libs/local.sh
@@ -4,15 +4,13 @@ echo 'ℹ️  Setting up ℹ️'
 rm -rf dist && rm -rf node_modules
 yarn
 # yarn webpack
-mkdir -p dist/libs && cp -r src/ dist/libs
-## only local
-cp -r node_modules dist/libs
+mkdir -p dist/libs && cp -r src/ dist/libs && cp -r node_modules dist/libs
 echo '✅  Setting up complete ✅'
 
 
 echo 'ℹ️  Copying files to /opt ℹ️'
 sudo cp -r dist/libs /opt
-echo '✅  Copied fules to /opt ✅'
+echo '✅  Copied files to /opt ✅'
 
 
 echo 'ℹ️  Cleaning up ℹ️'
