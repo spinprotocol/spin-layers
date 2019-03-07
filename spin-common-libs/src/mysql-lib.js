46565c6b292b275f932ce63ad5b7cd6a698eb58c
Mustafa Morca <psychoplasma@gmail.com>
2019-03-07 15:47:57 +0900

    Set wt database credentials through environment variables

--- spin-common-libs/src/mysql-lib.js
+++ spin-common-libs/src/mysql-lib.js
@@ -1,10 +1,9 @@
-const wtDB = require('../wtdb-credentials');
 const mysql = require('serverless-mysql')({
     config: {
-        host     : wtDB.host,
-        database : wtDB.database,
-        user     : wtDB.user,
-        password : wtDB.password
+        host     : process.env.wtDdHost,
+        database : process.env.wtDbDatabase,
+        user     : process.env.wtDbUser,
+        password : process.env.wtDbPassword
     }
 });
 
