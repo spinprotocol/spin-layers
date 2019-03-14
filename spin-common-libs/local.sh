c39c74a4cd9a931287913b9ce9228308ecd6e591
Harace Moon <mym0846@gmail.com>
2019-03-14 12:56:13 +0900

    Add paging common module in dynamoDB common lib

--- spin-common-libs/local.sh
+++ spin-common-libs/local.sh
@@ -2,10 +2,10 @@
 
 echo 'ℹ️  Setting up ℹ️'
 rm -rf dist && rm -rf node_modules
-export PARENT_DIR=../../
-export DEPENDENCIES_DEST=./
-node ../scripts/package-json-merger.js
-yarn
+# export PARENT_DIR=../../
+# export DEPENDENCIES_DEST=./
+# node ../scripts/package-json-merger.js
+npm install
 # yarn webpack
 mkdir -p dist/libs && cp -r src/ dist/libs && cp -r node_modules dist/libs
 echo '✅  Setting up complete ✅'
