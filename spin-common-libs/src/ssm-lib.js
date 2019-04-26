679755f45b372c08441cb352ad265deca4f70bed
Brandon <gus6359@gmail.com>
2019-04-26 16:00:47 +0900

    add ssm lib

---
+++ spin-common-libs/src/ssm-lib.js
@@ -0,0 +1,17 @@
+const AWS = require('aws-sdk');
+const ssm = new AWS.SSM({ region: 'ap-northeast-2' });
+const kms = new AWS.KMS({ region: 'ap-northeast-2' });
+// const Buffer = require('buffer')
+
+const getValue = (keyObj, callback) => {
+    return ssm.getParameter(keyObj).promise()
+}
+
+const decryptSecureValue = key => {
+    return kms.decrypt(key).promise()
+}
+
+exports.getParamterStoreValue = name => getValue({
+    Name: name,
+    WithDecryption: false
+})
