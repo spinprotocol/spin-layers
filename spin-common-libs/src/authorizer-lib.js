c0e31472df137bd3bf0fa8c659b916ebef9d018e
Brandon <gus6359@gmail.com>
2019-07-22 14:48:28 +0900

    remove authorizer and add authorizer-lib

--- spin-common-libs/src/authorizer-lib.js
+++ spin-common-libs/src/authorizer-lib.js
@@ -7,7 +7,16 @@ Object.assign(global, require('ffp-js'));
  * @returns { Boolean } true || false
  * @description: `tokenInfo` 의 auth 가 `accessAuthList`에 해당하는지 검사
  */
-exports.isAuthorized = (accessAuthList, tokenInfo) = go(
+exports.isAuthorized = (tokenInfo, ...accessAuthList) => go(
   accessAuthList,
+  flatL,
+  takeAll,
   a => a.some(auth => auth === tokenInfo.auth)
 )
+
+exports.USER_ROLE = {
+  all: ['influencer', 'admin', 'supplier'],
+  influencer: ['influencer'],
+  admin: ['admin'],
+  supplier: ['supplier']
+}
