b6bd09699346a5ce2a31b76417d2509c59cac1fe
Brandon <gus6359@gmail.com>
2019-09-09 11:53:39 +0900

    모듈 네이밍 변경(mql2 -> fxsql) & postgresql utils function 추가

--- spin-common-libs/src/postgresql-lib.js
+++ spin-common-libs/src/postgresql-lib.js
@@ -5,9 +5,38 @@ const config = {
     database: process.env.DB_DATABASE
 }
 
-const { PostgreSQL } = require('mql2');
+const { PostgreSQL } = require('fxsql');
 const { CONNECT } = PostgreSQL;
 
 const POOL = CONNECT(config);
 
 exports.POOL = POOL;
+
+const UTILS = {};
+
+/**
+ * @name getArrayToStringForQuery
+ * @param { Array }
+ * @description String 배열을 받아 배열의 String 값을 반환 해주는 함수.
+ * @example
+ *  ['a','b','c'] -> '["a","b","c"]'
+ */
+UTILS.getArrayToStringForQuery = array => `["${array.join('","')}"]`;
+
+/**
+ * @name getPathInAppliedInfluencers
+ * @param { Number, String, Sql }
+ */
+UTILS.getPathInAppliedInfluencers = (campaign_id, email, selectSql) => POOL.SQL`
+  SELECT
+    ${selectSql}
+  FROM
+    campaign,
+    jsonb_array_elements(applied_influencers) 
+    WITH ORDINALITY AS arr(applied_influencer, index)
+  WHERE
+    id = ${campaign_id} AND 
+    applied_influencer->>'email' = ${email}::TEXT
+`;
+
+exports.UTILS = UTILS
