5bce10795c1b3bfe81c71e807fbf6faf92ed6846
mason <mason@spinprotocol.io>
2019-12-17 11:16:53 +0900

    update node version

--- spin-common-libs/deploy.sh
+++ spin-common-libs/deploy.sh
@@ -10,7 +10,7 @@ npm install
 ## spin-common-libs Layer
 mkdir -p dist/commonLibs/libs && cp -r src/ dist/commonLibs/libs && cp -r node_modules dist/commonLibs/libs
 ## spin node_modules Layer
-mkdir -p dist/nodeDependencies/nodejs/node8 && cp -r node_modules dist/nodeDependencies/nodejs/node8
+mkdir -p dist/nodeDependencies/nodejs/node10 && cp -r node_modules dist/nodeDependencies/nodejs/node10
 echo '✅  Setting up complete ✅'
 
 echo 'ℹ️  Deploying ℹ️'
--- spin-common-libs/src/response-lib.js
+++ spin-common-libs/src/response-lib.js
@@ -1,58 +1,59 @@
-const { curry, go, tap, log } = require('ffp-js')
-exports.success = curry((headers, body) => buildResponse(200, body, headers));
-exports.failure = curry((headers, body) => buildResponse(500, body, headers));
+const { curry, go } = require('ffp-js')
+
+exports.success = curry((headers, body) => buildResponse(200, body, headers))
+exports.failure = curry((headers, body) => buildResponse(500, body, headers))
 
 const accessOriginList =
   [
-      'http://localhost:8080',
-      'https://dev.spin-protocol.com',
-      'https://staging.spin-protocol.com',
-      'https://spinprotocol.com',
-      'http://dalpumae20181.godomall.com',
-      'https://womanstalk.co.kr',
-      'http://womanstalk.co.kr'
-  ];
+    'http://localhost:8080',
+    'https://dev.spin-protocol.com',
+    'https://staging.spin-protocol.com',
+    'https://spinprotocol.com',
+    'http://dalpumae20181.godomall.com',
+    'https://womanstalk.co.kr',
+    'http://womanstalk.co.kr'
+  ]
 
 const accessHostList =
   [
-      'localhost:3000',
-      'localhost:3001',
-      'localhost:3002',
-      'localhost:3003',
-      'localhost:3004',
-      'localhost:3005',
-      'localhost:3006',
-      'localhost:3007',
-      'api.dev.spin-protocol.com',
-      'api.staging.spin-protocol.com',
-      'api.spinprotocol.com',
-      'api.womanstalk.co.kr'
+    'localhost:3000',
+    'localhost:3001',
+    'localhost:3002',
+    'localhost:3003',
+    'localhost:3004',
+    'localhost:3005',
+    'localhost:3006',
+    'localhost:3007',
+    'api.dev.spin-protocol.com',
+    'api.staging.spin-protocol.com',
+    'api.spinprotocol.com',
+    'api.womanstalk.co.kr'
   ]
 
-function buildResponse(statusCode, body, headers) {
-    return go(
-      accessOriginList,
-      list => !headers
-        ?  ({
-          statusCode: 200,
-          body: 'access schedule'
-        })
-        : go(
-          list,
-          list => list.some(a => a === headers.origin),
-          bool => !bool && !accessHostList.some(a => a === headers.Host)
-            ? ({
-              statusCode: 405,
-              body: 'not allowed access'
+function buildResponse (statusCode, body, headers){
+  return go(
+    accessOriginList,
+    list => !headers
+      ?  ({
+        statusCode: 200,
+        body: 'access schedule'
+      })
+      : go(
+        list,
+        list => list.some(a => a === headers.origin),
+        bool => !bool && !accessHostList.some(a => a === headers.Host)
+          ? ({
+            statusCode: 405,
+            body: 'not allowed access'
           })
           : ({
-              statusCode: statusCode,
-              headers: {
-                  "Access-Control-Allow-Origin": headers.origin,
-                  "Access-Control-Allow-Credentials": true,
-              },
-              body: JSON.stringify(body)
+            statusCode: statusCode,
+            headers: {
+              'Access-Control-Allow-Origin': headers.origin,
+              'Access-Control-Allow-Credentials': true,
+            },
+            body: JSON.stringify(body)
           })
-        )
-    )
+      )
+  )
 }
